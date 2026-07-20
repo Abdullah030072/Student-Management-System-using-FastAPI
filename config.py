fb8584556ab389288c13ea1f544c2f49e3708ba1
Abdullah030072 <abdullah030072@gmail.com>
2026-07-20 15:55:50 +0500

    Add the Faker Data Parameters

---
+++ config.py
@@ -0,0 +1,19 @@
+from faker import Faker
+
+# CSV File Name
+FILE_NAME = "students.csv"
+
+# Faker Object
+fake = Faker()
+
+# Available Courses
+COURSES = [
+    "Python",
+    "Java",
+    "C++",
+    "AI",
+    "Data Science",
+    "Web Development",
+    "Cyber Security",
+    "FastAPI"
+]
