659004fbab13ceff55d9978c87993df45683b330
Abdullah030072 <abdullah030072@gmail.com>
2026-07-21 19:44:01 +0500

    Resolve main.py merge conflict

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import csv
-<<<<<<< HEAD
 import uvicorn
 
 from fastapi import FastAPI, HTTPException
@@ -15,17 +14,6 @@ from student_service import (
 from menu import main_menu
 
 
-
-=======
-
-from fastapi import FastAPI
-
-from config import FILE_NAME
-from models import Student
-from menu import main_menu
-
-
->>>>>>> origin/fastapi
 # ==========================================
 # FastAPI App
 # ==========================================
@@ -37,10 +25,6 @@ app = FastAPI(
 )
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/fastapi
 # ==========================================
 # Home API
 # ==========================================
@@ -53,14 +37,8 @@ def home():
     }
 
 
-<<<<<<< HEAD
-
 # ==========================================
 # Get All Students
-=======
-# ==========================================
-# View All Students
->>>>>>> origin/fastapi
 # ==========================================
 
 @app.get("/students")
@@ -73,53 +51,27 @@ def get_students():
         reader = csv.DictReader(file)
 
         for row in reader:
-
             students.append(row)
 
     return students
 
 
-<<<<<<< HEAD
-
 # ==========================================
 # Get Student By ID
-=======
-# ==========================================
-# Search Student By ID
->>>>>>> origin/fastapi
 # ==========================================
 
 @app.get("/students/{student_id}")
 def get_student(student_id: int):
 
-<<<<<<< HEAD
     student = get_student_by_id(student_id)
 
     if student:
-
         return student
-
 
     raise HTTPException(
         status_code=404,
         detail="Student Not Found"
     )
-
-=======
-    with open(FILE_NAME, "r") as file:
-
-        reader = csv.DictReader(file)
-
-        for row in reader:
-
-            if int(row["ID"]) == student_id:
-
-                return row
-
-    return {
-        "message": "Student Not Found"
-    }
->>>>>>> origin/fastapi
 
 
 # ==========================================
@@ -131,7 +83,6 @@ def add_student_api(student: Student):
 
     with open(FILE_NAME, "r") as file:
 
-<<<<<<< HEAD
         reader = csv.DictReader(file)
 
         for row in reader:
@@ -143,23 +94,11 @@ def add_student_api(student: Student):
                     detail="Student ID Already Exists"
                 )
 
-=======
-        reader = csv.reader(file)
-
-        for row in reader:
-
-            if len(row) > 0 and int(row[0]) == student.id:
-
-                return {
-                    "message": "Student ID Already Exists"
-                }
->>>>>>> origin/fastapi
 
     with open(FILE_NAME, "a", newline="") as file:
 
         writer = csv.writer(file)
 
-<<<<<<< HEAD
         writer.writerow(
             [
                 student.id,
@@ -170,25 +109,12 @@ def add_student_api(student: Student):
             ]
         )
 
-=======
-        writer.writerow([
-            student.id,
-            student.name,
-            student.age,
-            student.course,
-            student.gpa
-        ])
->>>>>>> origin/fastapi
 
     return {
         "message": "Student Added Successfully"
     }
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/fastapi
 # ==========================================
 # Generate Fake Students
 # ==========================================
@@ -196,11 +122,6 @@ def add_student_api(student: Student):
 @app.post("/students/fake")
 def fake_students():
 
-<<<<<<< HEAD
-=======
-    from student_service import generate_fake_students
-
->>>>>>> origin/fastapi
     generate_fake_students()
 
     return {
@@ -208,10 +129,6 @@ def fake_students():
     }
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/fastapi
 # ==========================================
 # Update Student
 # ==========================================
@@ -223,10 +140,7 @@ def update_student_api(student_id: int, student: Student):
 
     found = False
 
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/fastapi
     with open(FILE_NAME, "r") as file:
 
         reader = csv.reader(file)
@@ -235,10 +149,7 @@ def update_student_api(student_id: int, student: Student):
 
         rows.append(header)
 
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/fastapi
         for row in reader:
 
             if int(row[0]) == student_id:
@@ -252,8 +163,6 @@ def update_student_api(student_id: int, student: Student):
                     student.course,
                     student.gpa
                 ]
-
-<<<<<<< HEAD
 
             rows.append(row)
 
@@ -278,28 +187,6 @@ def update_student_api(student_id: int, student: Student):
     }
 
 
-
-=======
-            rows.append(row)
-
-    if found:
-
-        with open(FILE_NAME, "w", newline="") as file:
-
-            writer = csv.writer(file)
-
-            writer.writerows(rows)
-
-        return {
-            "message": "Student Updated Successfully"
-        }
-
-    return {
-        "message": "Student Not Found"
-    }
-
-
->>>>>>> origin/fastapi
 # ==========================================
 # Delete Student
 # ==========================================
@@ -311,10 +198,7 @@ def delete_student_api(student_id: int):
 
     found = False
 
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/fastapi
     with open(FILE_NAME, "r") as file:
 
         reader = csv.reader(file)
@@ -323,10 +207,7 @@ def delete_student_api(student_id: int):
 
         rows.append(header)
 
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/fastapi
         for row in reader:
 
             if int(row[0]) == student_id:
@@ -335,10 +216,8 @@ def delete_student_api(student_id: int):
 
                 continue
 
-<<<<<<< HEAD
 
             rows.append(row)
-
 
 
     if not found:
@@ -362,43 +241,16 @@ def delete_student_api(student_id: int):
 
 
 
-
 # ==========================================
 # Run Terminal Menu OR FastAPI
-=======
-            rows.append(row)
-
-    if found:
-
-        with open(FILE_NAME, "w", newline="") as file:
-
-            writer = csv.writer(file)
-
-            writer.writerows(rows)
-
-        return {
-            "message": "Student Deleted Successfully"
-        }
-
-    return {
-        "message": "Student Not Found"
-    }
-
-
-# ==========================================
-# Run Terminal Program
->>>>>>> origin/fastapi
 # ==========================================
 
 if __name__ == "__main__":
 
-<<<<<<< HEAD
     print("\n====================================")
     print(" Student Management System ")
     print("====================================\n")
 
     print("Starting Terminal Menu...\n")
 
-=======
->>>>>>> origin/fastapi
     main_menu()
